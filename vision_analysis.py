eef26144d577faaac0cbab311f63c1e4fc258b8e
gdgrin <genedgrinberg@gmail.com>
2018-02-05 16:03:50 -0500

    write photo descriptions to file

--- vision_analysis.py
+++ vision_analysis.py
@@ -12,20 +12,26 @@ from google.cloud.vision import types
 client = vision.ImageAnnotatorClient()
 
 # The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'photos/CalUXVuWAAAeaqK.jpg')
+i=2
+while i < 10 :
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        'photos/image{}.jpg'.format(i))
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-image = types.Image(content=content)
+    image = types.Image(content=content)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
 
-print('Labels:')
-for label in labels:
-    print(label.description)
+    file = open("descriptions.txt","a")
+
+    for label in labels:
+        file.write(label.description)
+        file.write("\n")
+
+    i+=1

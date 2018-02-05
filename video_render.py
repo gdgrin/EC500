3586e44dc18a427ebe978e81db583931f8b20802
gdgrin <genedgrinberg@gmail.com>
2018-02-05 16:05:24 -0500

    updated file naming

--- video_render.py
+++ video_render.py
@@ -5,11 +5,11 @@ def image_rename():
     directory = "photos"
     for filename in os.listdir(directory):
         if filename.endswith(".jpg"):
-            os.rename(directory + "/" + filename, directory + "/" + str(i) + ".jpg")
+            os.rename(directory + "/" + filename, directory + "/image" + str(i) + ".jpg")
             i+=1
     return
 
 image_rename()
 
 fps = 1
-subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "photos/%d.jpg","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "video.avi"])
+subprocess.call(["ffmpeg","-y","-r",str(fps),"-i", "photos/image%d.jpg","-vcodec","mpeg4", "-qscale","5", "-r", str(fps), "video.avi"])

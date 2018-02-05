be254e4fc4299a1e6042d152400725c636d15084
gdgrin <genedgrinberg@gmail.com>
2018-02-05 18:25:50 -0500

    added comment

--- main.py
+++ main.py
@@ -6,16 +6,15 @@ import os
 
 import subprocess
 
-api_key = ''
-api_secret = ''
-access_token = ''
-access_secret = ''
+api_key = '07J6DTOfFNVvROefk2L4X3EPi'
+api_secret = 'y7zBiXCN8wSV8Uhn4wlYFjgxxFzRAasCpUEgD5jrx9QYFhnGgb'
+access_token = '4108052595-kCEtiUcDB2zeRqjn61wJqbsO1ZJ8jLwQRw7s1zR'
+access_secret = 'frHKcMR2mcy54NEHyv0eZ4yChoc4eB75ls4akvutoWp8O'
 
 auth = tweepy.OAuthHandler(api_key,api_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth)
-
 
 handle = raw_input("Enter Twitter Handle: \n ")
 
